b39122dd27e86d96da353754790fd87ea471bbbf
Saurabh Sharma <saurabh777@gmail.com>
2019-12-26 12:35:01 +0530

    Grains

--- grains/grains.go
+++ grains/grains.go
@@ -1,1 +1,25 @@
 package grains
+
+import (
+	"errors"
+	"math"
+)
+
+
+func Total() (sum uint64) {
+	for i := 1; i< 65; i++ {
+		sum += uint64(math.Pow(2, float64(i - 1)))
+	}
+	return sum
+}
+
+func Square(squares int) (grains uint64, err error) {
+	if squares <= 0 || squares > 64{
+		return 0, errors.New("invalid input")
+	}
+
+	
+	grains = uint64(1 * (math.Pow(2, float64(squares - 1))))
+
+	return grains, nil
+}
