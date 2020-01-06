b3470de25c76461c195ddade529c115850093937
Saurabh Sharma <saurabh777@gmail.com>
2020-01-06 11:25:39 +0530

    6thJan2020

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,8 +1,8 @@
 package tree
 
 import (
-	"fmt"
 	"sort"
+	"errors"
 )
 
 // From the test cases.
@@ -10,63 +10,109 @@ type Record struct {
 	ID, Parent int
 }
 
+// Node structure
 type Node struct {
 	ID       int
 	Children []*Node
 }
 
-/**
- * AddToParent
- */
-func addToParent(tree *Node, node *Node, parentID int) {
 
-	added := false
-	if tree != nil {
-		for {
-			if parentID != tree.ID {
-				chil := tree.Children
-				for _, e := range chil {
-					if e.ID == parentID {
-						e.Children = append(e.Children, node)
-						added = true
-						return
-					}
-				}
+// A recursive approach to check if the new Node to add exists.
+func containsTheNode(tree *Node, nodeId int) bool {
+	if tree == nil {
+		return false
+	} 
 
-				if added == false {
-					// The element parent could not be located.
-				}
-			}
-		}
+	traverse := tree
+	if traverse.ID == nodeId {
+		return true
 	}
 
-	// Add the element as the first element itself.
-	tree = node
+	for _, val := range traverse.Children {
+		return containsTheNode(val, nodeId)
+	}
+
+	return false
 }
 
+func addToTheParent(head *Node, newNode *Node, parent int) (*Node, error)  {
+	if head != nil {
+		if head.ID == parent {
+			head.Children = append(head.Children, newNode)
+			children := head.Children
+			sort.Slice(children, func(i,j int) bool {
+				return children[i].ID < children[j].ID
+			})
+			return head, nil
+		} else {
+			for _, val := range head.Children {
+				if val.ID == parent {
+					val.Children = append(val.Children, newNode)
+					children := val.Children
+					sort.Slice(children, func(i,j int) bool {
+						return children[i].ID < children[j].ID
+					})
+					return head, nil
+				}
+			}
+			// No parent found for this child, in the head.
+			return nil, errors.New(" error. ")
+		}
+	} else {
+		// The parent is not yet added.
+		head = &Node { 
+			parent,
+			nil ,
+		}
+
+		return addToTheParent(head, newNode, parent)
+	}
+	return head, nil
+}
+
+// Build - Builds the tree.
 func Build(records []Record) (tree *Node, err error) {
-
-	var newNode *Node
-
-	if records == nil || len(records) == 0 {
+	// The records only contain an ID number and a parent ID number. 
+	// var newNode *Node
+	
+	if records == nil {
 		return nil, nil
 	}
-
-	// First sort the element in the list so that the no parent is always the first.
-	sort.Slice(records, func(i, j int) bool {
-		return records[i].Parent < records[j].Parent
+	
+	// Sort the elements by index.
+	sort.Slice(records, func(i,j int) bool {
+		return records[i].ID < records[j].ID
 	})
 
-	fmt.Printf(" The object : %v", records)
+	// Iterate through the records and add one by one.
+	for i, v := range records {
 
-	for _, r := range records {
-		if tree == nil {
-			tree = &Node{r.ID, []*Node{}}
+		if containsTheNode(tree, v.ID) {
+			return nil, errors.New(" exists")
+		}
+		
+		if i != v.ID {
+			return nil, errors.New(" non continuous")
+		}
+
+		// Iterate through the records presented.
+		if v.ID == v.Parent {
+			// Root has the parent == id
+			if tree == nil {
+				// Root element found.
+				tree = &Node{v.ID, nil, }
+			}
 		} else {
-			newNode = &Node{r.ID, []*Node{}}
-			addToParent(tree, newNode, r.Parent)
+
+			if tree == nil {
+				return nil, errors.New(" root not found ")
+			}
+			
+
+			tree, _ = addToTheParent(tree, &Node{v.ID, nil,}, v.Parent)
 		}
 	}
+
 
 	return tree, nil
 }
