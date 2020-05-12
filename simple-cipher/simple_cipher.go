2ad0f197c1b945276a01dd417a6d0882bf92d048
Saurabh Sharma <saurabh777@gmail.com>
2020-05-12 18:47:27 +0530

    Refined simple cipher

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -7,149 +7,87 @@ import (
 	"strings"
 )
 
+// Vigenere Stores the key
+type Vigenere struct {
+	key string
+}
+
 // RegularExp Elements to find
 var RegularExp = regexp.MustCompile(`[^A-Za-z]`)
 
-// Encrypt struct
-type Encrypt struct {
-	distance int
-	key      string
-}
-
 // NewCaesar Object that implements the interface Cipher
 func NewCaesar() Cipher {
-	return Encrypt{distance: 3, key: ""}
+	return Vigenere{key: "d"}
 }
 
 // NewShift Object that implements the interface Cipher
 func NewShift(distance int) Cipher {
 	// Corner case when of 0, 26, -26
-	if distance < -26 || distance > 26 || distance == 0 {
+	if distance <= -26 || distance >= 26 {
 		return nil
 	}
-	return Encrypt{distance, ""}
+	return NewVigenere(string(mapChar('a', int32(distance))))
 }
 
 // NewVigenere Object that implements the interface Cipher
-func NewVigenere(key string) Cipher {
+func NewVigenere(k string) Cipher {
 
-	if key == "" || checkKeyInvalid(key) {
+	if regexp.MustCompile(`[^a-z]`).MatchString(k) || regexp.MustCompile(`^a*$`).MatchString(k) {
 		return nil
 	}
-	return Encrypt{0, key}
+
+	return Vigenere{key: k}
 }
 
-// checkAllAs Checks for all A's in the key.
-func checkKeyInvalid(s string) (f bool) {
-
-	// If any invalid character matches
-	if matched, _ := regexp.MatchString(`[^a-z]`, s); matched {
-		return true
+func mapChar(s rune, r int32) (e rune) {
+	switch {
+	case s < 'a'-r:
+		e = s + 26 + r
+	case s > 'z'-r:
+		e = (s - 26 + r)
+	default:
+		e = s + r
 	}
-
-	for _, m := range s {
-		switch m {
-		case 'a':
-			f = true
-		default:
-			f = false
-			return
-		}
-	}
-	return
+	return e
 }
 
-// removeAllOtherCharcterBeforeEncoding replace all the other elements
-func removeAllOtherCharcterBeforeEncoding(s string) (o string) {
+// stripString replace all the other elements
+func stripString(s string) (o string) {
 	o = RegularExp.ReplaceAllString(s, "")
 	log.Printf(" String: %s", o)
 	o = strings.ToLower(o)
 	return
 }
 
-func transformWithString(s string, e string, flag bool) string {
+// Encode Implements the interface Cipher
+func (e Vigenere) Encode(s string) (o string) {
+
 	var sb strings.Builder
 
-	em := []rune(e)
-
-	for i, sm := range s {
-		// The key has to be repeated
-		var d rune = 0
-
-		if i < len(em) {
-			d = em[i] - 'a'
-		} else {
-			d = em[(i%len(em))] - 'a'
-		}
-
-		if flag {
-			switch {
-			case sm >= 'a' && sm <= 'z':
-				fmt.Fprintf(&sb, "%s", string('a'+(sm-'a'+d)%26))
-			}
-		} else {
-			switch {
-			case sm >= 'a' && sm <= 'z':
-				diff := ((sm - 'a' - d) % 26)
-				newChar := ""
-
-				if diff < 0 {
-					newChar = string('z' + diff + 1)
-				} else {
-					newChar = string('a' + diff)
-				}
-				fmt.Fprintf(&sb, "%s", newChar)
-			}
-		}
-	}
-	return sb.String()
-}
-
-// transformWithDistance Allows transformation based on distance.
-func transformWithDistance(s string, d int) string {
-
-	rotateFunction := func(r rune) rune {
-		switch {
-		case r >= 'a' && r <= 'z':
-			val := (r - 'a' + rune(d))
-			if val < 0 && d < 0 {
-				return 'z' + ((val + 1) % 26)
-			}
-			return 'a' + (val % 26)
-
-		default:
-			log.Printf(" Not a charcter: %s", string(r))
-		}
-		return ' '
-	}
-
-	return strings.Map(rotateFunction, s)
-
-}
-
-// Encode Implements the interface Cipher
-func (e Encrypt) Encode(s string) (o string) {
 	o = ""
-	s = removeAllOtherCharcterBeforeEncoding(s)
+	s = stripString(s)
 
 	if s == "" || len(s) <= 0 {
 		return
 	}
 
+	log.Printf(" Encode: %s", s)
 	// Iterate throught the runes
 
-	switch {
-	case e.distance != 0:
-		o = strings.ReplaceAll(transformWithDistance(s, e.distance), " ", "")
-	default:
-		o = transformWithString(s, e.key, true)
+	for i, m := range s {
+		newChar := mapChar(m, int32(e.key[i%len(e.key)]-'a'))
+		fmt.Fprintf(&sb, "%c", newChar)
 	}
 
+	o = sb.String()
+	log.Printf(" Encoded: %s", o)
 	return
 }
 
 // Decode decides the encrypted string
-func (e Encrypt) Decode(s string) (o string) {
+func (e Vigenere) Decode(s string) (o string) {
+	var sb strings.Builder
+
 	o = ""
 
 	log.Printf(" Decode: %s", s)
@@ -158,13 +96,13 @@ func (e Encrypt) Decode(s string) (o string) {
 		return
 	}
 
-	switch {
-	case e.distance != 0:
-		log.Printf(" Distance: %d", e.distance)
-		o = transformWithDistance(s, -e.distance)
-	default:
-		o = transformWithString(s, e.key, false)
+	for i, m := range s {
+		newChar := mapChar(m, -int32(e.key[i%len(e.key)]-'a'))
+		fmt.Fprintf(&sb, "%c", newChar)
 	}
+
+	o = sb.String()
+	log.Printf(" Decoded: %s", o)
 
 	return
 }
